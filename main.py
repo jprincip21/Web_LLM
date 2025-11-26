8ce8793d36a58548d148c2080557d0101b0ebae3
jprincip21 <jprincip21@gmail.com>
2025-11-25 20:02:22 -0500

    Web Interface

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 import os
+import gradio as gr
 
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
+
 
 
 def main():
@@ -21,12 +23,14 @@ def main():
     Answer in 2-6 sentences.
     """
 
+    # General Setup for our llm
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.5-flash",
         google_api_key=gemini_api_key,
         temperature=0.5
     )
 
+    # Seting up our prompt to be sent to the chatbot.
     prompt = ChatPromptTemplate.from_messages(
         [("system", system_prompt),
         (MessagesPlaceholder(variable_name="history")),
@@ -34,17 +38,63 @@ def main():
     )
 
     chain = prompt | llm | StrOutputParser()
-    history = [] # Variable name for message placeholder
 
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == "exit":
-            break
-        else:
-            response = chain.invoke({"input": user_input, "history": history})
-            print("Albert: " + response)
-            history.append(HumanMessage(content=user_input))
-            history.append(AIMessage(content=response))
+    def chat(user_input, hist):
+        """
+        Creates a chat to be used with gradio chatbot function.
+        Every time a message is sent a history list is constructed to give the chatbot context.
+        Response takes the user input and history to invoke our chain.
+        """
+        langchain_history = []
+        for item in hist:
+            if item["role"] == "user":
+                langchain_history.append(HumanMessage(content=item["content"]))
+            elif item["role"] == "history":
+                langchain_history.append(AIMessage(content=item["content"]))
+
+        response = chain.invoke({"input": user_input, "history": langchain_history})
+
+        return "", hist + [{"role": "user", "content": user_input},
+                            {"role": "assistant", "content": response}]
+
+    def clear_chat():
+        """
+        Returns an empty string for msg and an empty list for history which will clear the chat.
+        """
+        return "", []
+
+    # Gradio setup
+    page = gr.Blocks(
+        title="Chat with Einstein"
+    )
+
+    # Add widgets to page.
+    with page:
+        # Basic title and description
+        gr.Markdown(
+            """
+            # Chat with Einstein
+            Welcome to your chat with Einstein
+            """
+        )
+
+        # Chatbot widget to allow for conversation wit bot
+        chatbot = gr.Chatbot(
+            avatar_images=[None, "einstein.png"],
+            show_label=False
+        )
+
+        # Textbox widget for user input
+        msg = gr.Textbox(show_label=False, placeholder="Ask Einstein!")
+
+        # When a message is submitted our chat function is called sending the msg and chatbot variables.
+        msg.submit(chat, [msg, chatbot], [msg, chatbot])
+
+        # Clear Button which clears outputs and history.
+        clear = gr.Button("Clear Chat", variant="secondary")
+        clear.click(clear_chat, outputs=[msg, chatbot])
+
+        page.launch(share=True)
 
 if __name__ == "__main__":
     main()
